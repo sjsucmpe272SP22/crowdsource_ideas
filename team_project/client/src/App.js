fbb4a81f7e26f9356d531a7cd3fe1e574a6044c2
roger9kuo <roger9kuo@berkeley.edu>
2022-04-19 15:48:55 -0700

    basic navbar, active func not working

--- team_project/client/src/App.js
+++ team_project/client/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './navbar.css'
 
 function App() {
   return (
     <div className="App">
+      <ul id='navbar'>
+        <li><a class="active" href="#ideas">Ideas</a></li>
+        <li><a href="#guide">Guide</a></li>
+        <li><a href="#soon">Coming Soon</a></li>
+        <li><a href="#announces">Annoucements</a></li>
+        <li style={{float: 'right'}}><a href="#profile">Profile</a></li>
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +29,18 @@ function App() {
     </div>
   );
 }
+
+// // Get the container element
+// var navbar = document.getElementById("navbar");
+// // Get all buttons inside the container
+// var btns = navbar.getElementsByTagName("li");
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current.classList.remove('active')
+//     this.className += "active";
+//   });
+// }
 
 export default App;
