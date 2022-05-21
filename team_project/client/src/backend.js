ae572bb41b5440c9e3af18f6c1412120ca141b29
Siddhant Satish Parmar <54446378+sparmar15@users.noreply.github.com>
2022-05-21 12:16:35 -0700

    Update backend.js

--- team_project/client/src/backend.js
+++ team_project/client/src/backend.js
@@ -3,6 +3,6 @@
 // Once you are done and want to push DON'T FORGET to change it back to API = "http://35.89.120.177:3001"
 
 // const API = "http://localhost:3001";
-const API = "http://35.89.120.177:3001";
+const API = "http://54.218.147.228:3001";
 
 export default API;
